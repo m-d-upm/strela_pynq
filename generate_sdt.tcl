529e874a3a69e8ba424d23fe8b76412893d2081c
m-d-upm <milos.dordevic@upm.es>
2026-05-19 16:16:01 +0200

    pulled patches into forked repo from main tha fix the device tree overlay generating script

--- generate_sdt.tcl
+++ generate_sdt.tcl
@@ -1,4 +1,3 @@
-
 if { $argc != 2 } {
     puts "Error: Incorrect number of arguments."
     puts "Usage: xsct generate_sdt.tcl <path_to_xsa> <output_directory>"
@@ -11,7 +10,7 @@ set output_dir [lindex $argv 1]
 sdtgen set_dt_param -dir $output_dir -xsa $xsa_file
 
 # sdtgen set_dt_param -board_dts zcu102-rev1.0
-# sdtgen set_dt_param -user_dts path/to/system-user.dtsi
+# sdtgen set_dt_param -include_dts {path/to/system-user.dtsi}
 # sdtgen set_dt_param -trace enable -debug enable
 
 sdtgen generate_sdt
--- process_support_archive.sh
+++ process_support_archive.sh
@@ -1,19 +1,69 @@
-#!/bin/sh
+#!/usr/bin/awk -f
 
-set -e
+# This script converts the first device tree in the second one. It essentially
+# peels the first two levels of the device tree and embeds the remaining content
+# in a device tree overlay.
 
-support_archive_name=$1
+# / {
+#         amba_pl: amba_pl {
+#                 (...)
+#         };
+# };
 
-xsct generate_sdt.tcl "${support_archive_name}.xsa" sdt_out
-# sed '1i /dts-v1/;\n/plugin/;' sdt_out/pl.dtsi | dtc -@ -I dts -O dtb -o "${support_archive_name}.dtbo" -
-awk -f make_overlay.awk sdt_out/pl.dtsi | tee pl.dts | dtc -@ -I dts -O dtb -o "${support_archive_name}.dtbo" -
-unzip -o "${support_archive_name}.xsa" "${support_archive_name}.bit"
+# /dts-v1/;
+# /plugin/;
+#
+# / {
+#         fragment@0 {
+#                 target-path = "/fpga-full";
+#                 __overlay__ {
+#                         (...)
+#                 };
+#         };
+# };
 
-cat << EOF > bitstream.bif
-the_ROM_image:
-{
-    "${support_archive_name}.bit"
+
+# https://docs.kernel.org/driver-api/fpga/fpga-region.html
+# https://www.kernel.org/doc/Documentation/devicetree/bindings/fpga/fpga-region.txt
+
+function push_if_eq(n) {
+    if (level == n) { level++ } else { print "bad" >> "/dev/stderr"; exit 1 }
 }
-EOF
 
-bootgen -image bitstream.bif -arch zynq -process_bitstream bin -w on
+function pop_if_lt(n) {
+    if (level < n) { level-- } else { print "bad" >> "/dev/stderr"; exit 1 }
+}
+
+function check_eq(n) {
+    if (level == n) { ; } else { print "bad" >> "/dev/stderr"; exit 1 }
+}
+
+BEGIN {
+    print "/dts-v1/;\n/plugin/;\n"
+    print "/ {"
+    print "\tfragment@0 {"
+    print "\t\ttarget-path = \"/fpga-full\";"
+    print "\t\t__overlay__ {"
+
+    level = 0
+}
+
+/^\/ \{/                 { push_if_eq(0); next }
+/^\tamba_pl: amba_pl \{/ { push_if_eq(1); next }
+/^\t{0,1}\};$/           { pop_if_lt(3);  next }
+/^\t*$/                  { next }
+                         { check_eq(2);
+    # This should only work after having matched /^\t\tafi0.*{$/
+    if (match($0, /^\t\t\tcompatible = "xlnx,afi-fpga";$/)) {
+        print "\t\t\t\tcompatible = \"xlnx,zynq-afi-fpga\";"
+    } else {
+        print "\t" $0
+    }
+}
+
+END {
+    check_eq(0);
+    print "\t\t}; /* __overlay__ */"
+    print "\t}; /* fragment@0 */"
+    print "}; /* / */"
+}
